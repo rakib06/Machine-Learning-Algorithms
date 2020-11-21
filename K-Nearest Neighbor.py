52bcc2a133fe11f2cdfdb0e9b4c0f4349c23740a
Rakibul Hasan <six.rakib@gmail.com>
2020-11-21 21:45:47 +0600

    adding git ignore

--- K-Nearest Neighbor.py
+++ K-Nearest Neighbor.py
@@ -10,11 +10,28 @@ class KNearestNeighbor():
 
     def predict(self, X_test):
         distances = self.compute_distance(X_test)
+        return self.predict_labels(distances)
 
-    def compute_distance(self):
-        pass
+    def compute_distance(self, X_test):
+        # Naive, inefficient way
+        num_test = X_test.shape[0]
+        num_train = self.X_train.shape[0]
+        distances = np.zeros((num_test, num_train))
 
-    def predict_labels(self, distance):
-        pass
+        for i in range(num_test):
+            for j in range(num_train):
+                distances[i,j] = np.sqrt(np.sum((X_test[i,:]-self.X_train[j,:]))**2)
+        return distances
+        
 
+    def predict_labels(self, distances):
+    num_test = distances.shape[0]
+    y_pred = np.zeros(num_test)
+
+    for i in range(num_test):
+        y_indices  = np.argsort(distances[i,:])
+        k_closest_classes = self.Y_train[:self.k]]
+
+if if __name__ == "__main__":
+    pass
 
